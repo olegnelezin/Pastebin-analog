c43a54ab1956da556dc624a8ea10886139035ffc
Oleg Nelezin <astravsu@gmail.com>
2024-07-07 10:03:04 +0300

    init

---
+++ src/main/java/org/example/PastebinApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastebinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PastebinApplication.class);
+    }
+}
