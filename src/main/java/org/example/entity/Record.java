2e607a6848716985cff45827a51de895e9ffca07
Oleg Nelezin <astravsu@gmail.com>
2024-07-08 08:57:08 +0300

    add jpa to entity

--- src/main/java/org/example/entity/Record.java
+++ src/main/java/org/example/entity/Record.java
@@ -1,10 +1,20 @@
 package org.example.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "record")
 public class Record {
 
+    @Id
     private Long id;
 
+    @Column
     private String url;
 
+    @Column
     private String minioUrl;
 }
